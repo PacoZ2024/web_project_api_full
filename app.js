9b722c3959f929004e19669f4984f815cc4ce45f
Megaman <pacosclub@gmail.com>
2026-01-18 20:29:39 -0600

    refactor(Sintaxis): Cambio de comillas dobles por simples

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const { createUser, login } = require("./controllers/users");
-const auth = require("./middleware/auth");
-const usersRoutes = require("./routes/users");
-const cardsRoutes = require("./routes/cards");
-const NotFoundError = require("./errors/not-found-err");
+const express = require('express');
+const mongoose = require('mongoose');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middleware/auth');
+const usersRoutes = require('./routes/users');
+const cardsRoutes = require('./routes/cards');
+const NotFoundError = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose
-  .connect("mongodb://localhost:27017/aroundb")
-  .then(() => console.log("Conectado a la base de datos!"))
+  .connect('mongodb://localhost:27017/aroundb')
+  .then(() => console.log('Conectado a la base de datos!'))
   .catch((err) => console.error(err));
 app.use(express.json());
-app.post("/signin", login);
-app.post("/signup", createUser);
-app.use("/users", auth, usersRoutes);
-app.use("/cards", auth, cardsRoutes);
-app.use("/", (req, res) => {
-  throw new NotFoundError("Recurso solicitado no encontrado");
+app.post('/signin', login);
+app.post('/signup', createUser);
+app.use('/users', auth, usersRoutes);
+app.use('/cards', auth, cardsRoutes);
+app.use('/', (req, res) => {
+  throw new NotFoundError('Recurso solicitado no encontrado');
 });
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message:
-      statusCode === 500 ? "Se ha producido un error en el servidor" : message,
+      statusCode === 500 ? 'Se ha producido un error en el servidor' : message,
   });
 });
 
